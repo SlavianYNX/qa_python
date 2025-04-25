b08c285f859a0804610fae020b7cf15ab3df3e7a
Slava Eremin <slavik.eremin@yandex.ru>
2025-04-25 10:28:07 +0300

    Add test_new_book_not_genre

--- tests.py
+++ tests.py
@@ -1,3 +1,5 @@
+import pytest
+
 from main import BooksCollector
 
 # класс TestBooksCollector объединяет набор тестов, которыми мы покрываем наше приложение BooksCollector
@@ -18,15 +20,25 @@ class TestBooksCollector:
 
         # проверяем, что добавилось именно две
         # словарь books_rating, который нам возвращает метод get_books_rating, имеет длину 2
-        assert len(collector.get_books_rating()) == 2
+        assert len(collector.get_books_genre()) == 2
 
-    # напиши свои тесты ниж
+    # напиши свои тесты ниже
     # чтобы тесты были независимыми в каждом из них создавай отдельный экземпляр класса BooksCollector()
+
+    def test_add_new_book_not_genre(self):
+        collector = BooksCollector()
+        book_one = 'Война и мир'
+        collector.add_new_book(book_one)
+        assert book_one in collector.books_genre
+        assert collector.books_genre[book_one] == ''
+
+
     def test_set_book_genre_new_genre_book(self):
+        collector = BooksCollector()
         book_one = 'Гордость и предубеждение и зомби'
         genre = 'Комедии'
-        collector = BooksCollector()
         collector.add_new_book(book_one)
-        collector.set_book_genre(book_one,genre)
+        collector.set_book_genre(book_one, genre)
         assert book_one in collector.books_genre
-        assert collector.set_book_genre(book_one) == genre
+        assert collector.get_book_genre(book_one) == 'Комедии'
+
